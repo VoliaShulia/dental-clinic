45e20013e5cbfb73af957e8d0eb601c6eec2395e
VoliaShulia <shulgan.lelya@mail.ru>
2023-05-05 18:36:24 +0400

    fix footer logo

--- src/sections/Footer/index.jsx
+++ src/sections/Footer/index.jsx
@@ -1,22 +1,30 @@
 import styles from './index.module.css'
 import Instagram from './instagram.svg' 
+import logo from './Logo.png'
 
 function Footer() {
     return (
         <div className={styles.footerWrapper}>
             <div className={styles.upper}>
-                <div className={styles.adressWrapper}>
-                    <h3 className={styles.adressTitle}>Адрес</h3>
-                    <p className={styles.adressParagraph}>г. Минск ул.Ноябрьякая 6, 2 этаж, каб. 100</p>
-                    <p className={styles.timeParagraph}>Ежедневно с 8:00 до 22:00</p>
+                <div className={styles.leftWrapper}>
+                    <div className={styles.adressWrapper}>
+                        <h3 className={styles.adressTitle}>Адрес</h3>
+                        <p className={styles.adressParagraph}>г. Минск ул.Ноябрьякая 60 Б,<br/>каб. 100</p>
+                        <p className={styles.timeParagraph}>С 8:00 до 22:00</p>
+                    </div>
                 </div>
-                <div className={styles.contactsWrapper}>
-                    <h3 className={styles.contactsTitle}>Контакты</h3>
-                    <a className={styles.email} href="shulgan.lelya@gmail.com">shulgan.lelya@gmail.com</a>
-                    <a className={styles.phone} href="+375292424294">+375292424294</a>
-                    <a className={styles.instagram} href="https://www.instagram.com/volia.a.o.f/">
-                    @Swilldent
-                        <img className={styles.instagramPicture} src={Instagram} alt="Instagram" /></a>
+                <div className={styles.logoWrapper}>
+                    <img className={styles.logoImg} src={logo}/>
+                </div>
+                <div className={styles.rightWrapper}>
+                    <div className={styles.contactsWrapper}>
+                        <h3 className={styles.contactsTitle}>Контакты</h3>
+                        <a className={styles.email} href="shulgan.lelya@gmail.com">shulgan.lelya@gmail.com</a>
+                        <a className={styles.phone} href="+375292424294">+375292424294</a>
+                        <a className={styles.instagram} href="https://www.instagram.com/volia.a.o.f/">
+                        @Swilldent
+                            <img className={styles.instagramPicture} src={Instagram} alt="Instagram" /></a>
+                    </div>
                 </div>
             </div>
             <div className={styles.down}>
